a5a48a1a4a4c9ac7de93b8dc8e37bdc89a981b99
Mario <marioguba14@gmail.com>
2024-04-19 17:57:41 +0200

    Inicialización

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import sys
+
 import pygame
 
 pygame.init()
-finestraJoc = pygame.display.set_mode()
+finestraJoc = pygame.display.set_mode(600, 400)
+rellotge = pygame.time.Clock()
+
+gameOver = False
+
+def DetectarEvents():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+def Pintar():
+    pygame.draw.rect(finestraJoc, ())
+
+while not gameOver:
+
+    DetectarEvents()
+    Pintar()
+
+    rellotge.tick(30)
+    pygame.display.update()
